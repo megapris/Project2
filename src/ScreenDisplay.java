659b41955021e62d59dd5c07bdf742eb0671661c
megapris <31768717+megapris@users.noreply.github.com>
2021-02-13 21:09:45 -0500

    Delete ScreenDisplay.java

--- src/ScreenDisplay.java
+++
@@ -1,9 +0,0 @@
-package project2GIVE_TO_STUDENTS;
-
-public enum ScreenDisplay {
-    CurrentRentalStatus,
-    ReturnedItems,
-    DueWithInWeek,
-    DueWithinWeekGamesFirst,
-    Cap14DaysOverdue
-}
